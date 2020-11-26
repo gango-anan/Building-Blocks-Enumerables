fa0634edd9f681de4609e7c08f66d894eaa9c6c7
gasb150 <gasb_150@hotmail.com>
2020-11-26 15:04:49 -0500

    reduce code lines  my_select  method

--- enumerables.rb
+++ enumerables.rb
@@ -28,17 +28,8 @@ module Enumerable
     arr=self
     n= arr.length-1
     pass=0
-     i=0
-     n_array=[]
-     pass=0
-     for i in 0..n do
-       x=arr[i]    
-       if yield(x)
-         n_array[pass] = x 
-         pass += 1        
-       end
-     end
+    n_array=[]
+    arr.my_each { |x| n_array << x if yield (x) }
     return n_array
   end
-  
 end
