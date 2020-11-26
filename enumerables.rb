2cae50525bd35e11cd11f5f892d88cda3bf038c9
gasb150 <gasb_150@hotmail.com>
2020-11-26 15:03:11 -0500

    code my_each_with index  method

--- enumerables.rb
+++ enumerables.rb
@@ -10,4 +10,17 @@ module Enumerable
     end
     return self
   end
+
+  def my_each_with_index
+    arr=self.to_a
+    n= arr.length-1
+    i=0
+    n_array=[]
+    for i in 0..n do
+    x=arr[i]
+    n_array[i] = yield(x,i)
+    end
+    return self
+  end
+  
 end
