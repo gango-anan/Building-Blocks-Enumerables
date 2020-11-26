8844848eb2f722fc47cfbe1662a542565b90c9c3
gasb150 <gasb_150@hotmail.com>
2020-11-26 15:02:47 -0500

    code my_ each method

---
+++ enumerables.rb
@@ -0,0 +1,13 @@
+module Enumerable
+  def my_each
+    arr=self.to_a
+    n= arr.length-1
+    i=0  
+    n_array=[]
+     for i in 0..n do
+     x=arr[i]
+     n_array[i] = yield(x)
+    end
+    return self
+  end
+end
