aaebc450061dafe7de900f890f2327a502649f7a
gasb150 <gasb_150@hotmail.com>
2020-11-27 15:38:26 -0500

    add readme file

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Building-Blocks-Enumerables
-This project involves creating some commonly used enumerable methods
+This project involves creating some commonly used enumerable methods (#my_each, #my_each_with _index, #my_select, #my_all, #my_any?, #my_none, #my_count, #my_map, #my_inject), stored on a script file, the new methods are onto the existing Enumerable module, #my_inject was tested creating a method called #multiply_els.
+
+## Built With
+
+- Ruby
+
+## Live Demo
+
+
+
+## Authors
+
+👨🏻‍💻 **Galiwango Ananiya** 
+
+- GitHub: [@gango-anan](https://github.com/gango-anan) 
+- Twitter: [@gango_anan](https://twitter.com/gango_anan) 
+- LinkedIn: [@galiwango-ananiya](https://www.linkedin.com/in/galiwango-ananiya-0800821b4/) 
+
+:bird: **Gustavo Sanmartin**
+
+- GitHub: [@gasb150](https://github.com/gasb150)
+- Twitter: [@7aves](https://twitter.com/7aves)
+- LinkedIn: [Gutavo Sanmartin](https://www.linkedin.com/in/gustavo-sanmartin-b3b68261/)
+
+## Acknowledgments
+
+- [Microverse](https://www.microverse.org/)
+- [The Odin Project](https://www.theodinproject.com)
+- [Ruby-doc](https://ruby-doc.org)
+
+## Show your support
+
+<p> Give a 
+  <g-emoji class="g-emoji" alias="star" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2b50.png"><img class="emoji" alt="star" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/2b50.png"></g-emoji>
+  if you like this project!</p>
+
+
+## License
+  <p>This project is <a href="../feature/LICENSE">MIT</a> licensed.</p>
--- enumerables.rb
+++ enumerables.rb
@@ -1,100 +1,83 @@
 module Enumerable
   def my_each
-    arr=self.to_a
-    n= arr.length-1
-    i=0  
-    n_array=[]
-     for i in 0..n do
-     x=arr[i]
-     n_array[i] = yield(x)
-    end
-    return self
+    arr = to_a
+    n = arr.length
+    i = 0
+    n_array = []
+    while i < n
+      x = arr[i]
+      n_array[i] = yield(x)
+      i += 1
+   end
+    self
   end
 
   def my_each_with_index
-    arr=self.to_a
-    n= arr.length-1
-    i=0
-    n_array=[]
-    for i in 0..n do
-    x=arr[i]
-    n_array[i] = yield(x,i)
+    arr = to_a
+    n = arr.length
+    i = 0
+    n_array = []
+    while i < n
+      x = arr[i]
+      n_array[i] = yield(x, i)
+      i += 1
     end
-    return self
+    self
   end
 
-  
   def my_select
-    arr=self
-    n= arr.length-1
-    pass=0
-    n_array=[]
-    arr.my_each { |x| n_array << x if yield (x) }
-    return n_array
+    arr = self
+    n_array = []
+    arr.my_each { |x| n_array << x if yield x }
+    n_array
   end
 
-  def my_all?(arg=nil)
-    arr=self
-    n= arr.length-1
-    y= true
+  def my_all?(arg = nil)
+    arr = self
+    y = false
     if block_given?
-    arr.my_each do |x|
-      if yield (x) 
-        y = true
-      else
-        y = false
+      arr.my_each do |x|
+        y = if yield x
+              true
+            else
+              false
+            end
         return y
       end
-    end
-   elsif !block_given? && arg==nil
-    y=true
-   else
-    arr.my_each do |x|
-      if x != arg
-        y=false
-      else
-        y=true
+    elsif !block_given? && arg.nil?
+      y = true
+    else
+      arr.my_each do |x|
+        y = x == arg
+      end
     end  
-   end
-   end
-    return y
-
+    y
   end
 
-  def my_any?(arg=nil)
-    arr=self
-    n= arr.length-1
-    pass=0
-    n_array=[]
-    y=false
-    if block_given? 
+  def my_any?(arg = nil)
+    arr = self
+    y = false
+    if block_given?
       arr.my_each do |x|
-        if yield (x)
+        if yield x
           y = true
           return y
         else
-        y = false
+          y = false
         end
       end
       return y
-    elsif !block_given? && arg==nil
-       arr.my_each do |x|
-        if arr!=nil
-          y=true
-          return y
-        else
-          return y
-        end
-      end
-    else 
+    elsif !block_given? && arg.nil?
+      arr.my_each { return y = true unless arr.nil? }
+    else
       arr.my_each do |x|
         if x == arg
-          y=true
+          y = true
           return y
         end
       end
     end
-    return y
+    y
   end
 
 
@@ -104,25 +87,29 @@ module Enumerable
     pass=0
     n_array=[]
     y=true
+
     if block_given?
       arr.my_each do |x|
         if yield (x)
           y = false
-        return y
+          return y
+        else
+           y = true
+        end
+      
+    end
+    return y
+    
+    elsif !block_given? && arg==nil
+    arr.my_each do |x|
+      if x==nil || x==false       
+          y=true
       else
-        y = true
+        y=false
+        return y
       end
     end
     return y
-    elsif !block_given? && arg==nil
-    arr.my_each do |x|
-      if arr==nil
-        y=true
-        return y
-      else
-        return y
-      end
-    end
     else
     arr.my_each do |x|
       if x != arg
@@ -133,135 +120,65 @@ module Enumerable
     end
   end
 
-  def my_count(arg=nil)
-    arr=self
-    n= arr.length-1
-    pass=0
-    n_array=[]
-    y=0
+  def my_count(arg = nil)
+    arr = self
+    y = 0
     if block_given?
-      arr.my_each { |x| y += 1 if yield (x) }
-    elsif !block_given? && arg==nil
-     y=arr.length
-    else 
-     y=arr.my_select {|x| x==arg}.length
+      arr.my_each { |x| y += 1 if yield x }
+    elsif !block_given? && arg.nil?
+      y = arr.length
+    else
+      y = arr.my_select { |x| x == arg }.length
     end
-    return y
+    y
   end
 
-  def my_map
-    array=self.to_a
+  def my_map(arg = nil)
+    array = to_a
     index = 0
-    newArray = Array.new
-
-    while index < array.length
-      newArray[index] = yield(array[index])
-      index += 1
+    n_array = []
+    if arg.nil?
+      while index < array.length
+        n_array[index] = yield(array[index])
+        index += 1
+      end
+    else
+      while index < array.length
+        n_array[index] = yield(arg[index])
+        index += 1
+      end
     end
 
-    newArray
+    n_array
   end
-
-  # def my_inject (arg1=nil, arg2=nil)
-  #   array=self.to_a
-  #   index = 1
-  #   sum = array[0]
-  #   if block_given?
-  #     while index < array.length
-  #       if array[index].is_a?(Numeric)
-  #         sum = yield(sum, array[index])
-  #         index += 1
-  #       else
-  #         return sum
-  #       end
-  #     end
-  #     if arg1!=nil
-  #       yield(sum,arg1)
-  #     end
-  #     sum
-  #   end
-  # end
 
   def my_inject(arg1 = nil, arg2 = nil)
-    arg2, arg1 = arg1, nil if (arg1 != nil && arg2 == nil) && (arg1.is_a?(Symbol) || arg1.is_a?(String))
-    
-    if !block_given? && arg2 != nil 
-      my_each do |element| 
-        if arg1 == nil
-          arg1 = element
-        else
-          arg1 = arg1.send(arg2, element)
-        end
+    if (!arg1.nil? && arg2.nil?) && (arg1.is_a?(Symbol) || arg1.is_a?(String))
+      arg2 = arg1
+      arg1 = nil
+    end
+
+    if !block_given? && !arg2.nil?
+      my_each do |element|
+        arg1 = if arg1.nil?
+                 element
+               else
+                 arg1.send(arg2, element)
+               end
       end
     else
       my_each do |element|
-        if  arg1 == nil
-          arg1 = element 
-        else
-          arg1 = yield(arg1, element)
-        end
+        arg1 = if arg1.nil?
+                 element
+               else
+                 yield(arg1, element)
+               end
       end
     end
     arg1
   end
-
 end
 
-puts '1.--------my_each--------'
- %w[Sharon Leo Leila Brian Arun].my_each { |friend| puts friend }
-
- puts '2.--------my_each_with_index--------'
- %w[Sharon Leo Leila Brian Arun].my_each_with_index { |friend, index| puts friend if index.even? }
-
- puts '3.--------my_select--------'
- puts (%w[Sharon Leo Leila Brian Arun].my_select { |friend| friend != 'Brian' })
-
-puts '4.--------my_all--------'
-puts (%w[ant bear cat].my_all? { |word| word.length >= 3 }) #=> true
-puts (%w[ant bear cat].my_all? { |word| word.length >= 4 }) #=> false
-puts %w[ant bear cat].my_all?(/t/) #=> false
-puts [1, 2i, 3.14].my_all?(Numeric) #=> true
-puts [].my_all? #=> true
-
-puts '5.--------my_any--------'
-puts (%w[ant bear cat].my_any? { |word| word.length >= 3 })
-puts (%w[ant bear cat].my_any? { |word| word.length >= 4 }) #=> true
-puts %w[ant bear cat].my_any?(/d/) #=> false
-puts [nil, true, 99].my_any? #=> true
-puts [].my_any? #=> false
-
-puts '6.--------my_none--------'
-puts (%w[ant bear cat].my_none? { |word| word.length == 5 }) #=> true
-puts (%w[ant bear cat].my_none? { |word| word.length >= 4 }) #=> false
-puts %w[ant bear cat].my_none?(/d/) #=> true
-puts [].my_none? #=> true
-puts [nil].my_none? #=> true
-puts [nil, false].my_none? #=> true
-puts [nil, false, true].my_none? #=> false
-
-puts '7.--------my_count--------'
-arr = [1, 2, 4, 2]
-puts arr.my_count #=> 4
-puts arr.my_count(2) #=> 2
-puts (arr.my_count { |x| (x % 2).zero? }) #=> 3
-
- puts '8.--------my_maps--------'
- my_order = ['medium Big Mac', 'medium fries', 'medium milkshake']
- puts (my_order.my_map { |item| item.gsub('medium', 'extra large') })
- puts ((0..5).my_map { |i| i * i })
- puts 'my_map_proc'
-
-puts '8.--------my_inject--------'
-puts ((1..5).my_inject { |sum, n| sum % n }) #=> 15
-puts (1..5).my_inject(1) { |product, n| product * n }
-longest = %w[ant bear cat].inject do |memo, word|
-  memo.length > word.length ? memo : word
-  end
-  puts longest 
-
-  def multiply_els(elements)
-    elements.my_inject(:*)
-  end
-
-  p "multiply_els"
-p multiply_els([2, 4, 5])
+def multiply_els(elements)
+  elements.my_inject(:*)
+end
