1bd4fabb3f292e47be8276dd9eb44c18677262a2
Galiwango Ananiya <galiwango101@gmail.com>
2020-12-16 18:45:23 +0300

    add my_each spec

---
+++ spec/enumerables_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../enumerables'
+
+describe "enumerables" do
+  let(:my_array) { [1, 2] }
+  let(:my_hash) { { min: 1, max: 9 } }
+  let(:my_range) { (1..3) }
+  describe "my_each:" do
+    it 'should return an Enumerator if block is not given' do
+      expect(my_array.my_each).to be_a(Enumerator)
+    end
+
+    it 'should return a list if block is given' do
+      expect(my_array.my_each{ |elem| puts elem}).to eql(my_array) 
+    end
+
+    it 'should return a list(array) if block is given' do
+      expect(my_array.my_each{ |elem| puts elem}).to eql(my_array) 
+    end
+
+    it 'should return a list(hash) if block is given' do
+      expect(my_hash.my_each{ |k, v| puts "Key: #{k} , Value: #{v}"}).to eql(my_hash) 
+    end
+
+    it 'should return a range if block is given' do
+      expect(my_range.my_each{ |elem| puts elem }).to equal(my_range) 
+    end
+  end
+end
